d96777ddf1e54c92003b766559ca7b4fa9174f32
Adam Piech <adampiech7@op.pl>
2016-04-17 21:31:57 +0200

    Dodano test.

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -1,12 +1,26 @@
 package edu.iis.mto.time;
 
-import static org.junit.Assert.*;
-
+import edu.iis.mto.time.Order;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(OrderTimeFactory.class)
 public class OrderTest {
 
+    @Test
+    public void test() {
+        mockStatic(OrderTimeFactory.class);
+        OrderTime timeSource = new OrderTime();
+        timeSource.getTime().plusHours(20);
+        when(OrderTimeFactory.getOrderTime()).thenReturn(timeSource);
+        Order order = new Order();
+        order.submit();
+        order.confirm();
+    }
 }
